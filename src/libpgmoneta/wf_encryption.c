bef9cf730be946d69f8b3d0806889468f8252b3a
Jichen Xu <kyokitisin@gmail.com>
2022-09-02 19:47:27 +0900

    feat: implement execution of encryption workflow

--- src/libpgmoneta/wf_encryption.c
+++ src/libpgmoneta/wf_encryption.c
@@ -27,20 +27,23 @@
  */
 
 /* pgmoneta */
-#include <node.h>
-#include <pgmoneta.h>
+#include <aes.h>
 #include <info.h>
 #include <link.h>
 #include <logging.h>
+#include <node.h>
+#include <pgmoneta.h>
 #include <utils.h>
 #include <workflow.h>
 
 /* system */
-#include <stdatomic.h>
+// #include <stdbool.h>
+// #include <stdatomic.h>
 #include <stdlib.h>
 
 static int encryption_setup(int, char*, struct node*, struct node**);
 static int encryption_execute(int, char*, struct node*, struct node**);
+static int decryption_execute(int, char*, struct node*, struct node**);
 static int encryption_teardown(int, char*, struct node*, struct node**);
 
 struct workflow*
@@ -67,7 +70,7 @@ pgmoneta_workflow_encryption(bool encrypt)
    return wf;
 }
 
-static int 
+static int
 encryption_setup(int server, char* identifier, struct node* i_nodes, struct node** o_nodes)
 {
    return 0;
@@ -76,16 +79,169 @@ encryption_setup(int server, char* identifier, struct node* i_nodes, struct node
 static int
 encryption_execute(int server, char* identifier, struct node* i_nodes, struct node** o_nodes)
 {
+   char* d = NULL;
+   char* to = NULL;
+   char* prefix = NULL;
+   char* directory = NULL;
+   char* id = NULL;
+   char* compress_suffix = NULL;
+   char* tarfile = NULL;
+   time_t encrypt_time;
+   int total_seconds;
+   int hours;
+   int minutes;
+   int seconds;
+   char elapsed[128];
+   struct configuration* config;
+
+   config = (struct configuration*)shmem;
+
+   if (i_nodes != NULL)
+   {
+      prefix = pgmoneta_get_node_string(i_nodes, "prefix");
+
+      if (!strcmp(prefix, "Restore"))
+      {
+         to = pgmoneta_get_node_string(*o_nodes, "to");
+         d = malloc(strlen(to) + 1);
+         memset(d, 0, strlen(to) + 1);
+         memcpy(d, to, strlen(to));
+      }
+      else if (!strcmp(prefix, "Archive"))
+      {
+         directory = pgmoneta_get_node_string(i_nodes, "directory");
+         id = pgmoneta_get_node_string(i_nodes, "id");
+
+         d = pgmoneta_append(d, directory);
+         d = pgmoneta_append(d, "/");
+         d = pgmoneta_append(d, config->servers[server].name);
+         d = pgmoneta_append(d, "-");
+         d = pgmoneta_append(d, id);
+         switch (config->compression_type)
+         {
+            case COMPRESSION_GZIP:
+               compress_suffix = ".tar.gz";
+               break;
+            case COMPRESSION_ZSTD:
+               compress_suffix = ".tar.zstd";
+               break;
+            case COMPRESSION_LZ4:
+               compress_suffix = ".tar.lz4";
+               break;
+            default:
+               pgmoneta_log_error("encryption_execute: Unknown compression type");
+               break;
+         }
+         d = pgmoneta_append(d, compress_suffix);
+         d = pgmoneta_append(d, ".aes");
+         if (pgmoneta_exists(d))
+         {
+            pgmoneta_delete_file(d);
+         }
+      }
+      else
+      {
+         d = pgmoneta_get_server_backup_identifier_data(server, identifier);
+      }
+   }
+   else
+   {
+      d = pgmoneta_get_server_backup_identifier_data(server, identifier);
+   }
+
+   encrypt_time = time(NULL);
+
+   if (i_nodes != NULL)
+   {
+      if (!strcmp(prefix, "Archive"))
+      {
+         tarfile = pgmoneta_get_node_string(*o_nodes, "tarfile");
+         tarfile = pgmoneta_append(tarfile, compress_suffix);
+         pgmoneta_encrypt_file(tarfile, d);
+      }
+      else
+      {
+         pgmoneta_encrypt_data(d);
+      }
+   }
+   else
+   {
+      pgmoneta_encrypt_data(d);
+   }
+
+   total_seconds = (int)difftime(time(NULL), encrypt_time);
+   hours = total_seconds / 3600;
+   minutes = (total_seconds % 3600) / 60;
+   seconds = total_seconds % 60;
+
+   memset(&elapsed[0], 0, sizeof(elapsed));
+   sprintf(&elapsed[0], "%02i:%02i:%02i", hours, minutes, seconds);
+
+   pgmoneta_log_debug("Encryption: %s/%s (Elapsed: %s)", config->servers[server].name, identifier, &elapsed[0]);
+
+   free(d);
+
    return 0;
 }
 
+static int
 decryption_execute(int server, char* identifier, struct node* i_nodes, struct node** o_nodes)
 {
-    return 0;
+   char* d = NULL;
+   char* to = NULL;
+   char* prefix = NULL;
+   time_t decrypt_time;
+   int total_seconds;
+   int hours;
+   int minutes;
+   int seconds;
+   char elapsed[128];
+   struct configuration* config;
+
+   config = (struct configuration*)shmem;
+
+   if (i_nodes != NULL)
+   {
+      prefix = pgmoneta_get_node_string(i_nodes, "prefix");
+
+      if (!strcmp(prefix, "Restore"))
+      {
+         to = pgmoneta_get_node_string(*o_nodes, "to");
+         d = malloc(strlen(to) + 1);
+         memset(d, 0, strlen(to) + 1);
+         memcpy(d, to, strlen(to));
+      }
+      else
+      {
+         d = pgmoneta_get_server_backup_identifier_data(server, identifier);
+      }
+   }
+   else
+   {
+      d = pgmoneta_get_server_backup_identifier_data(server, identifier);
+   }
+
+   decrypt_time = time(NULL);
+
+   pgmoneta_decrypt_data(d);
+
+   total_seconds = (int)difftime(time(NULL), decrypt_time);
+   hours = total_seconds / 3600;
+   minutes = (total_seconds % 3600) / 60;
+   seconds = total_seconds % 60;
+
+   memset(&elapsed[0], 0, sizeof(elapsed));
+   sprintf(&elapsed[0], "%02i:%02i:%02i", hours, minutes, seconds);
+
+   pgmoneta_log_debug("Decryption: %s/%s (Elapsed: %s)", config->servers[server].name, identifier, &elapsed[0]);
+
+   free(d);
+
+   return 0;
 }
 
-static int 
+static int
 encryption_teardown(int server, char* identifier, struct node* i_nodes, struct node** o_nodes)
 {
-    return 0;
+   return 0;
 }
